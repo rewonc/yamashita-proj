44e31c675cba66d6c3191551b9d9c4086e18d12f
rewonc <rewonc@gmail.com>
2014-05-02 09:59:05 -0400

    not using sinatra

--- script.rb
+++
@@ -1,7 +0,0 @@
-require 'rubygems'
-require 'rmagick'
-
-Magick::Image.read('5x5circle.jpg')[0].each_pixel do |pixel, col, row|
-  puts "Pixel at: #{col}x#{row}:
-  \tR: #{pixel.red}, G: #{pixel.green}, B: #{pixel.blue}"
-end

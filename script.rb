131b5607c04ffc369244f598dfa7b457e9524524
rewonc <rewonc@gmail.com>
2014-05-03 12:45:49 -0400

    added nail board, beginning drawing algo

--- script.rb
+++ script.rb
@@ -2,11 +2,64 @@ require 'rubygems'
 require 'rmagick'
 require 'delegate'
 
+=begin
+So the photo is around 600 pixels wide and 700 pixels high
+Say I want the nails every half inch. 
+I also see the end product being like 2 ft x 3 ft. 
+That means I have 24 x 28 nails. 
+Or, approximately 25 pixels per nail.
+25 x 25 pixels per nail. 
+If that's the nail grid, it seems like if the board were 25x25 nail grids
+it would allow for maximum extension from the edge grids.
+
+Hmm. what does it look like from the middle?
+=end
+
 class PixelDecorator < SimpleDelegator
   def rgb
     # see http://stackoverflow.com/questions/687261/converting-rgb-to-grayscale-intensity
     lightness = (red/65535.0 * 0.2126) + (green/65535.0 * 0.7152) + (blue/65535.0 * 0.0722)
-    (lightness * 100).round
+    percentage = (lightness * 100).round
+    100 - percentage
+  end
+end
+
+class GraphDecorator < SimpleDelegator
+  
+  def initialize(image)
+    super
+    @x_spacing = Math.sqrt(columns).round.to_i
+    @y_spacing = Math.sqrt(rows).round.to_i
+    x_mod = columns - @x_spacing*@x_spacing
+    y_mod = rows - @y_spacing*@y_spacing
+    if x_mod >= 0
+      @x_columns = @x_spacing
+    else
+      @x_columns = @x_spacing - 1
+    end
+    if y_mod >= 0
+      @y_rows = @y_spacing
+    else
+      @y_rows = @y_spacing - 1
+    end
+  end
+
+  def spacing
+    "You'll have a #{@x_spacing}x#{@y_spacing} set of pixels between nails. There are #{@x_columns} columns and #{@y_rows} rows of nails"
+  end
+
+  def array
+    arr = []
+    (0..@x_columns).each do |column|
+      (0..@y_rows).each do |row|
+        arr << [column*@x_spacing + 1, row*@y_spacing + 1]
+      end
+    end
+    arr
+  end
+
+  def first_point
+    array[0]
   end
 end
 
@@ -41,12 +94,10 @@ class Imagemap
     map
   end  
   
-  def display  
-    Magick::Image.read('5x5circle.jpg')[0].each_pixel do |pixel, col, row|
-      puts "Pixel at: #{col}x#{row}:
-      \tR: #{pixel.red}, G: #{pixel.green}, B: #{pixel.blue}"
-    end
+  def graph  
+    GraphDecorator.new(image)
   end  
+
 end  
 
 
--- server.rb
+++ server.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 get '/hi' do
   load 'script.rb'
-  @imagemap = Imagemap.new('25x25star.jpg')
+  @imagemap = Imagemap.new('17x33multistar.jpg')
   erb :index
 end
 
